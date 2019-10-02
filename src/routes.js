18ac11103302218b3e5031be8d5a7a4069261f6a
SamuelJacome <samuelvictor@rede.ulbra.br>
2019-10-02 17:15:16 -0300

    att

--- src/routes.js
+++ src/routes.js
@@ -14,6 +14,7 @@ const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionController.store);
 
+routes.get('/spots', SpotController.index);
 routes.post('/spots', upload.single('thumbnail'), SpotController.store);
 
 
